fb2890f67d00302dbac58b5909f7d8159634f4e2
x-yongz <yongzx90@gmail.com>
2022-10-16 20:55:56 +0800

    Run shell scripts

---
+++ run.sh
@@ -0,0 +1,6 @@
+echo "This script is going to create a file name file.txt"
+touch file.txt
+
+echo "This script is going to write something into the file"
+echo "Hello world!" >> file.txt
+echo "This is a test to run some scripts" >> file.txt
